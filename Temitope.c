e2c5ec8a8cc4c50fec061a5193860a0d6a937f51
Temmy-01 <69118025+Temmy-01@users.noreply.github.com>
2020-08-04 15:01:40 +0100

    Update Temitope.c

--- Temitope.c
+++ Temitope.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-    printf("Famakinwa Temitope Joseph,t_famakinwa@yahoo.com,C,Biosystematics and molecular identification, @Temmy-01");
+    printf("Famakinwa Temitope Joseph,t_famakinwa@yahoo.com,C,Biosystematics and molecular identification, @Temmy-01\n");
     return 0;
 }
