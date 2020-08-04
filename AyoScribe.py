d04804bcdd662f050162e820cb3483ea2703a65e
AyoScribe <69205339+AyoScribe@users.noreply.github.com>
2020-08-04 20:54:31 +0100

    Add files via upload

---
+++ AyoScribe.py
@@ -0,0 +1,4 @@
+#script to print details
+questions = {'name':'Olabanji Ayodele,', 'email':'olabanjiayodele5@gmail.com,', 'language':'Python,','biostack username':'@AyoScribe,','slack username':'@AyoScribe'}
+for q in questions.values():
+  print (q,'', end ='')
