c38896aadb7364e3a58e8e989f472b7596517c96
Indomitable-D <69111793+Indomitable-D@users.noreply.github.com>
2020-08-04 12:50:32 +0100

    Add files via upload

---
+++ Indomitable-D.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Name='Akinropo Isaac Damilola'
+email='indomitable.dammy@gmail.com'
+language='bash'
+biostack='genomics'
+username='@Indomitable-D'
+echo $Name','$email','$language','$biostack','$username
