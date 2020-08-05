7ac35006b71b77b8c50f9ab4302883dff8ea26d3
yomi-droid <69104688+yomi-droid@users.noreply.github.com>
2020-08-04 23:40:18 -1200

    Add files via upload

---
+++ yomi.py
@@ -0,0 +1,7 @@
+
+hack_me={"name":"yomi,", "mail":"abayomi.ag@gmail.com,", "language":"python,", "biostack":"vaccine informatics,", "id":"@yomi,"} 
+for i in hack_me.values():
+    print(i, '',end='')
+
+
+
